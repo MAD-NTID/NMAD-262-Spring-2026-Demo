5d8a30fd1908a2f49068879097a073386b4c2edc
Kemoy Campbell <kscics@rit.edu>
2026-02-02 08:26:56 -0500

    add the profile builder sample

---
+++ ProfileBuilder/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace ProfileBuilder;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void UpdateNamePreview(object? sender, EventArgs e)
+	{
+		//get the typed value
+		string typed = NameForm.Text;
+
+		//check if empty
+		if(string.IsNullOrWhiteSpace(typed))
+			return ; //do nothing
+
+		//otherwise we have something so we update the preview
+		NamePreview.Text = typed;
+	}
+
+
+}
