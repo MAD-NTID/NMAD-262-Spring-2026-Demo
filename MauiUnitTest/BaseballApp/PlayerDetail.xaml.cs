5fa08322c2f2a300c8d0b651be7a1d9124821e27
Kemoy Campbell <kemoycampbell45@gmail.com>
2026-03-30 09:51:37 -0400

    add view models

--- MauiUnitTest/BaseballApp/PlayerDetail.xaml.cs
+++ MauiUnitTest/BaseballApp/PlayerDetail.xaml.cs
@@ -1,36 +1,20 @@
 using System.ComponentModel;
+using BaseballApp.ViewModels;
 
 namespace BaseballApp;
 
-public partial class PlayerDetail : ContentPage, INotifyPropertyChanged
+public partial class PlayerDetail : ContentPage
 {
-	public  Player Player{get;set;}
-
-	//two different way to use property to determine if the player is a pitcher or batter
-	//long way
-	public bool IsPitcher
-	{
-		get
-		{
-			return Player is Pitcher;
-		}
-	}
-
-	//short way using lamda expression
-	public bool IsBatter => Player is Batter;
+	public PlayerDetailViewModel ViewModel;
 
 	public PlayerDetail(Player player)
 	{
+		ViewModel = new PlayerDetailViewModel(player);
 		InitializeComponent();
-		this.Player = player;
-
 
 		// set the binding context to the player object
-		BindingContext = this;
+		BindingContext = ViewModel;
 		
-		// //notify the UI that the player has changed so it can update the visibility of the stat sections
-		// OnPropertyChanged(nameof(IsPitcher));
-		// OnPropertyChanged(nameof(IsBatter));
 	}
 
 	public async void OnBackButtonClicked(object sender, EventArgs e)
