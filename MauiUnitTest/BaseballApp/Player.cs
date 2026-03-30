3cc185062668416b90641e1948b773e83148afd9
Kemoy Campbell <kemoycampbell45@gmail.com>
2026-03-30 08:39:35 -0400

    unit test example

---
+++ MauiUnitTest/BaseballApp/Player.cs
@@ -0,0 +1,7 @@
+public class Player
+{
+    public string Name{get;set;}
+    public int JerseyNumber{get;set;}
+    public string Team{get;set;}
+    public string ImageUrl{get;set;}
+}
---
+++ MauiUnitTest/BaseballApp/Storage.cs
@@ -0,0 +1,71 @@
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+
+public static class Storage
+{
+  private static readonly string filename = "players.json";
+  //public static readonly string filePath = Path.Combine(FileSystem.AppDataDirectory, filename);
+  public static string filePath;
+
+  public static void Init(string directory)
+  {
+    filePath = Path.Combine(directory, filename);
+  }
+
+  private static readonly JsonSerializerSettings settings = new JsonSerializerSettings
+  {
+      TypeNameHandling = TypeNameHandling.All, // include type info
+      Formatting = Formatting.Indented
+  };
+
+
+  /**
+  This method will load the json object from the file
+  */
+  public static async Task<List<Player>> LoadAsync()
+  {
+    //the file doesnt exist so we will just return a empty list 
+    if (!File.Exists(filePath))
+    {
+       return new List<Player>(); 
+    }
+
+    //Use the stream reader to read in the data from the file
+    StreamReader reader =  new StreamReader(filePath);
+
+    //read in the entire json string
+    string playerJsonData = reader.ReadToEnd();
+
+    //close the reader
+    reader.Close();
+
+    //convert the full json string to C# object with the deserialize object
+    List<Player> players = JsonConvert.DeserializeObject<List<Player>>(playerJsonData, settings);
+
+    //return the players object
+    return players;
+  }
+
+  public static async Task Save(List<Player> players)
+  {
+
+    //do nothing if the list is empty
+    if(players.Count == 0)
+        return ; // do nothing
+
+    //use the streamwriter to write to the file
+    StreamWriter writer = new StreamWriter(filePath);
+    
+    //convert the players objects to a JSON string representation
+    string jsonPlayers = JsonConvert.SerializeObject(players, settings);
+    
+    await writer.WriteAsync(jsonPlayers);
+    await writer.FlushAsync();
+
+    //close the writer when you are done
+    writer.Close();
+
+   
+        
+  }
+}
